2dbe1edca9ab51cc77c61abae2ac6e271bf992a2
Alexandra Westas <alexandra.westas1999@hotmail.com>
2026-02-05 15:52:17 +0100

    Added Enums

---
+++ Enums/Enum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAnalysis.Enums
+{
+    internal class Enum
+    {
+        public enum InsideOutsideMenu
+        {
+            Outside = 1,
+            Inside
+        }
+
+        public enum OutsideMenu
+        {
+            Averages = 1,
+            Warm_To_Cold,
+            Humidity_Dry_To_Wet,
+            Moldrisk_Low_To_High,
+            Meteorological_Autumn,
+            Meteorological_Winter
+
+        }
+
+        public enum InsdieMenu
+        {
+            Avereges = 1,
+            Warm_To_Cold,
+            Humidity_Dry_To_Wet,
+            Moldrisk_Low_To_High
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Grupp 8");
         }
     }
 }
