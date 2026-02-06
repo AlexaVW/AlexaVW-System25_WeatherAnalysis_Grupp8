00619baef776395dc3a662d47848ef0551c31452
Alexandra Westas <alexandra.westas1999@hotmail.com>
2026-02-06 09:45:25 +0100

    Restructured

---
+++ Graphics/UI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAnalysis.UI
+{
+    public class UI_Menu
+    {
+        public static void PrintMenu(Enum menuEnum)
+        {
+            //Get what type of enum was inserted
+            Type myEnum = menuEnum.GetType();
+            //Print all menu rows
+            foreach (int i in Enum.GetValues(myEnum))
+            {
+                //Print row value
+                Console.WriteLine("[" + i + "] " + Enum.GetName(myEnum, i).Replace('_', ' '));
+            }
+        }
+    }
+}
