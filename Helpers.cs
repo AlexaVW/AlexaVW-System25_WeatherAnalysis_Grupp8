034813070afd9d44d1d2421e9336fbff0d47831b
Alexandra Westas <alexandra.westas1999@hotmail.com>
2026-02-09 12:38:13 +0100

    .

--- Helpers.cs
+++ Helpers.cs
@@ -8,13 +8,7 @@ namespace WeatherAnalysis
 {
     public class Helpers
     {
-        public static string[] GetFileText(string fileNameWithExtension)
-        {
-            string projectPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\Data"));
-            string filePath = Path.Combine(projectPath, fileNameWithExtension);
-
-            return File.ReadAllLines(filePath);
-        }
+        
 
     }
 }
---
+++ Reader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAnalysis
+{
+    internal class Reader
+    {
+        public static string path = "../../../Data/";
+
+        public static string[] GetFileLines(string fileNameWithExtension)
+        {
+            string projectPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\Data"));
+            string filePath = Path.Combine(projectPath, fileNameWithExtension);
+
+            return File.ReadAllLines(filePath);
+        }
+    }
+}
